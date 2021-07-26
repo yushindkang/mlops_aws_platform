015afb21099e04975aa6e9f6823f8466369b6aca
Yushin Kang <yushink@splunk.com>
2021-07-26 11:12:57 +1000

    deploy lambda iam

--- main.tf
+++ main.tf
@@ -7,25 +7,25 @@ resource "aws_s3_bucket" "test_bucket" {
 }
 
 
-# resource "aws_iam_role" "iam_for_lambda" {
-#   name = "iam_for_lambda"
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
 
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Action": "sts:AssumeRole",
-#       "Principal": {
-#         "Service": "lambda.amazonaws.com"
-#       },
-#       "Effect": "Allow",
-#       "Sid": ""
-#     }
-#   ]
-# }
-# EOF
-# }
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
 
 resource "aws_lambda_function" "test_lambda" {
   filename      = "lambda_function_payload.zip"
