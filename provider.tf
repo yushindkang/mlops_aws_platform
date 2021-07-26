6561b43c73f989cffc6f4126e758cb4c0ad46796
Yushin Kang <yushink@splunk.com>
2021-07-26 10:51:16 +1000

    refacor

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }	
+}
+
+
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
