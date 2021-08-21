c2329d9be751a055c3379ccbbfdc608df5811665
Yushin Kang <yushink@splunk.com>
2021-08-21 10:10:52 +1000

    fix tf pipeline

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 terraform {
-    required_version = ">= 0.14.0"
+  required_version = ">= 0.14.0"
 }
 
 provider "aws" {
-    version = ">= 2.28.1"
-    region  = var.region
+  version = ">= 2.28.1"
+  region  = var.region
 }
