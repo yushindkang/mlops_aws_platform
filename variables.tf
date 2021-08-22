b64568f4fc03b34076ae58f535b0fadb822e8202
Yushin Kang <yushink@splunk.com>
2021-08-22 11:22:18 +1000

    remove canary, add admin

--- variables.tf
+++ variables.tf
@@ -43,14 +43,14 @@ variable "map_users" {
   }))
 
   default = [
+#     {
+#       userarn  = "arn:aws:iam::112227029085:user/canary"
+#       username = "canary"
+#       groups   = ["system:masters"]
+#     },
     {
-      userarn  = "arn:aws:iam::112227029085:user/canary"
-      username = "canary"
-      groups   = ["system:masters"]
-    },
-    {
-      userarn  = "arn:aws:iam::66666666666:user/user2"
-      username = "user2"
+      userarn  = "arn:aws:iam::112227029085:user/admin"
+      username = "admin"
       groups   = ["system:masters"]
     },
   ]
