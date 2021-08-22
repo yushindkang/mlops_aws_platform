6f66c0a6a8752b6065c25394f61f4d1801324cbe
Yushin Kang <yushink@splunk.com>
2021-08-22 17:56:45 +1000

    rm tf state config map

--- kubernetes.tf
+++ kubernetes.tf
@@ -4,13 +4,6 @@
 
 # The Kubernetes provider is included in this file so the EKS module can complete successfully. Otherwise, it throws an error when creating `kubernetes_config_map.aws_auth`.
 # You should **not** schedule deployments and services in this workspace. This keeps workspaces modular (one for provision EKS, another for scheduling Kubernetes resources) as per best practices.
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
