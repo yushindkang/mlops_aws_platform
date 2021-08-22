5073a5b6de59ff7cf6797fb79ea17dff0b8e547b
yushindkang <87850091+yushindkang@users.noreply.github.com>
2021-08-22 17:59:48 +1000

    flatten subnets (#11)
    * flatten subnets
    
    * test
    
    * manage aws auth false
    
    * copy data block to k8s.tf
    
    * rm tf state config map
    
    * typo
    
    Co-authored-by: Yushin Kang <yushink@splunk.com>

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,13 +1,19 @@
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = local.cluster_name
   cluster_version = "1.20"
-  subnets         = [module.vpc.private_subnets, module.vpc.public_subnets]
+  subnets         = flatten([module.vpc.private_subnets, module.vpc.public_subnets])
 
   tags = {
     Environment = "training"
-    GithubRepo  = "terraform-aws-eks"
-    GithubOrg   = "terraform-aws-modules"
   }
 
   vpc_id = module.vpc.vpc_id
@@ -34,10 +40,3 @@ module "eks" {
   ]
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
