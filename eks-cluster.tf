103746bda6a10e0f2d22b9558f9a16bf066cff4a
yushindkang <87850091+yushindkang@users.noreply.github.com>
2021-08-23 20:28:26 +1000

    change security group (#14)
    Co-authored-by: Yushin Kang <yushink@splunk.com>

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -32,7 +32,7 @@ module "eks" {
       name                          = "worker-group-1"
       instance_type                 = "t2.small"
       additional_userdata           = "echo foo bar"
-      asg_desired_capacity          = 2
+      asg_desired_capacity          = 1
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
       key_name                      = local.pem_mac_16
     },
@@ -40,7 +40,7 @@ module "eks" {
       name                          = "worker-group-2"
       instance_type                 = "t2.medium"
       additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
       asg_desired_capacity          = 1
       key_name                      = local.pem_mac_16
     },
