0fb0b7a122e19d0379b2730e9ccd60ed465d8d0c
Yushin Kang <yushink@splunk.com>
2021-08-24 17:32:32 +1000

    tf fmt

--- vpc.tf
+++ vpc.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "demo" {
   cidr_block = "10.0.0.0/16"
 
   tags = tomap({
-    "Name" = "terraform-eks-demo-node",
+    "Name"                                      = "terraform-eks-demo-node",
     "kubernetes.io/cluster/${var.cluster-name}" = "shared"
   })
 }
@@ -24,7 +24,7 @@ resource "aws_subnet" "demo" {
   vpc_id                  = aws_vpc.demo.id
 
   tags = tomap({
-    "Name" = "terraform-eks-demo-node",
+    "Name"                                      = "terraform-eks-demo-node",
     "kubernetes.io/cluster/${var.cluster-name}" = "shared"
   })
 }
